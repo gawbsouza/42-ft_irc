00113bb84fe3c5fb0f4a0bda1162ddbad53fe075
brwnovisky <brwnovisky@hotmail.com>
2024-05-24 18:16:58 -0300

    CCD-13 - new codes added

--- src/command/ResponseCode.hpp
+++ src/command/ResponseCode.hpp
@@ -6,25 +6,27 @@
 /*   By: bluiz-al <bluiz-al@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 21:00:07 by bluiz-al          #+#    #+#             */
-/*   Updated: 2024/05/23 22:00:45 by bluiz-al         ###   ########.fr       */
+/*   Updated: 2024/05/24 14:07:37 by bluiz-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RESPONSE_CODE_HPP
 # define RESPONSE_CODE_HPP
 
-#define SERVER_PREFIX 		":ft_irc"
-#define CRLF 				"\r\n"
-#define SPACE 				" "
+#define SERVER_PREFIX	":ft_irc"
+#define CRLF			"\r\n"
+#define SPACE			" "
 
 //messages
-#define MSG_AUTHENTICATED 	":You already authenticated"
-#define MSG_NEEDMOREPARAMS 	":Not enough parameters"
-#define MSG_PASSWDMISMATCH 	":Password incorrect"
+#define MSG_AUTHENTICATED		":You already authenticated"
+#define MSG_NEEDMOREPARAMS		":Not enough parameters"
+#define MSG_PASSWDMISMATCH		":Password incorrect"
+#define MSG_ALREADYREGISTERED	":You already registered"
 
 //error codes
-#define ERR_UNKNOWNERROR	"400"
-#define ERR_NEEDMOREPARAMS	"461" 
-#define ERR_PASSWDMISMATCH	"464" 
+#define ERR_UNKNOWNERROR		"400"
+#define ERR_NEEDMOREPARAMS		"461" 
+#define ERR_ALREADYREGISTERED	"462" 
+#define ERR_PASSWDMISMATCH		"464" 
 
 #endif
