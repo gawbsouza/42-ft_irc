54d66837e67ef9ee5e46da7ac778a555db92439e
brwnovisky <brwnovisky@hotmail.com>
2024-05-31 19:53:12 -0300

    CCD-23 - Implement TOPIC command

---
+++ src/command/TopicCommand.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   TopicCommand.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasouza <gasouza@student.42sp.org.br >     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 21:26:09 by gasouza           #+#    #+#             */
+/*   Updated: 2024/05/30 21:33:39 by gasouza          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOPIC_COMMAND_HPP
+# define TOPIC_COMMAND_HPP
+
+# include "Command.hpp"
+# include "../entity/UserService.hpp"
+# include "../entity/ChannelService.hpp"
+
+# define TOPIC_CMD "TOPIC"
+
+class TopicCommand: public Command
+{
+private:
+
+    ChannelService & _channelService;
+
+public:
+
+    TopicCommand(ChannelService & channelService);
+    ~TopicCommand(void);
+    
+    void execute(User & user, std::vector<std::string> args) const;
+};
+
+#endif
