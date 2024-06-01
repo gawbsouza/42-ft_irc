6ec9c640141beb62dfc88736658af95e5af49408
Gabriel Souza <email@gawbsouza.dev.br>
2024-06-01 20:28:39 -0300

    CCD-35 - improve UsersCommand class code organization

--- src/command/UsersCommand.hpp
+++ src/command/UsersCommand.hpp
@@ -6,15 +6,15 @@
 /*   By: gasouza <gasouza@student.42sp.org.br >     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 19:40:43 by gasouza           #+#    #+#             */
-/*   Updated: 2024/05/26 11:04:04 by gasouza          ###   ########.fr       */
+/*   Updated: 2024/06/01 20:18:14 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef USERS_COMMAND_HPP
 # define USERS_COMMAND_HPP
 
-#include "../entity/UserService.hpp"
-#include "../command/Command.hpp"
+# include "../entity/UserService.hpp"
+# include "../command/Command.hpp"
 
 class UsersCommand: public Command
 {
@@ -25,7 +25,7 @@ private:
 public:
 
     UsersCommand(UserService & service);
-    ~UsersCommand();
+    ~UsersCommand(void);
     
     void execute(User & user, std::vector<std::string> args) const;
     
